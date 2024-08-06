653e60232ce06402444b614464e048c8c2ac491a
Gun-Hee-SON <songunhee5426@gmail.com>
2024-08-06 15:05:41 +0900

    ver_01
    초기에 한번 옮겨봄

---
+++ sql_app/main4.py
@@ -0,0 +1,171 @@
+s
+# Records Output
+Date: 213123
+Location: 123123
+User ID: None
+Title: 123123
+Hashtag: None
+Content: None
+------
+Date: 111
+Location: 111
+User ID: 111
+Title: 111
+Hashtag: 11
+Content: 111
+------
+Date: 231
+Location: 123
+User ID: 21323
+Title: 123
+Hashtag: 3123
+Content: 123123
+------
+Date: ㄷㅂㄷ
+Location: ㅂㅈㄷㅂㅈ
+User ID: ㅂㅈㄷㅂ
+Title: ㅂㅈㄷ
+Hashtag: ㅂㅈㄷㅂㅈㄷ
+Content: ㅈㄷㅂㄷㅂㅈㄷ
+------
+Date: 123123
+Location: 1312312
+User ID: son
+Title: 3123d
+Hashtag: 따뜻함
+Content: 231132
+
+
+------
+Date: 123123
+Location: 21312
+User ID: 21312
+Title: 2312
+Hashtag: 2312
+Content: 213123
+------
+Date: 1111
+Location: 222
+User ID: 333
+Title: 333
+Hashtag: 333
+Content: 3333
+------
+Date: 23123
+Location: 12312
+User ID: 312312312
+Title: 123123
+Hashtag: 23123
+Content: 213123123
+------
+Date: 231321
+Location: 131231
+User ID: 123123
+Title: 21312
+Hashtag: 123123
+Content: 213123
+------
+Date: 3131
+Location: 12312
+User ID: 13231
+Title: 12312
+Hashtag: 31121
+Content: 31231232
+------
+
+# Records Output
+Date: 213123
+Location: 123123
+User ID: None
+Title: 123123
+Hashtag: None
+Content: None
+------
+Date: 111
+Location: 111
+User ID: 111
+Title: 111
+Hashtag: 11
+Content: 111
+------
+Date: 231
+Location: 123
+User ID: 21323
+Title: 123
+Hashtag: 3123
+Content: 123123
+------
+Date: ㄷㅂㄷ
+Location: ㅂㅈㄷㅂㅈ
+User ID: ㅂㅈㄷㅂ
+Title: ㅂㅈㄷ
+Hashtag: ㅂㅈㄷㅂㅈㄷ
+Content: ㅈㄷㅂㄷㅂㅈㄷ
+------
+Date: 123123
+Location: 1312312
+User ID: son
+Title: 3123d
+Hashtag: 따뜻함
+Content: 231132
+
+
+------
+Date: 123123
+Location: 21312
+User ID: 21312
+Title: 2312
+Hashtag: 2312
+Content: 213123
+------
+Date: 1111
+Location: 222
+User ID: 333
+Title: 333
+Hashtag: 333
+Content: 3333
+------
+Date: 23123
+Location: 12312
+User ID: 312312312
+Title: 123123
+Hashtag: 23123
+Content: 213123123
+------
+Date: 231321
+Location: 131231
+User ID: 123123
+Title: 21312
+Hashtag: 123123
+Content: 213123
+------
+Date: 3131
+Location: 12312
+User ID: 13231
+Title: 12312
+Hashtag: 31121
+Content: 31231232
+------
+
+# Records Output
+Date: 213123
+Location: 123123
+User ID: None
+Title: 123123
+Hashtag: None
+Content: None
+------
+Date: 111
+Location: 111
+User ID: 111
+Title: 111
+Hashtag: 11
+Content: 111
+------
+Date: 2023
+Location: 대전312
+User ID: son123
+Title: 대전 성심당1231
+Hashtag: 대전1232
+Content: 빵 맛있다2312
+------
